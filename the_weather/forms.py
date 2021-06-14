1b4085ce02ce4e5641fbb5427e899ab0492d5010
thm1d <tahmid11q@gmail.com>
2021-06-14 22:04:45 +0600

    User can add City

---
+++ the_weather/forms.py
@@ -0,0 +1,9 @@
+from django.db.models import fields
+from django.forms import ModelForm, TextInput, widgets
+from .models import City
+
+class CityForm(ModelForm):
+    class Meta:
+        model = City
+        fields = ['name']
+        widgets = {'name' : TextInput(attrs={'class' : 'input', 'placeholder' : 'City Name'})}
--- the_weather/views.py
+++ the_weather/views.py
@@ -1,22 +1,36 @@
 import requests
 from django.shortcuts import render
 from decouple import config
+from .models import City
+from .forms import CityForm
 
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + config('API_KEY')
-    city = 'Pabna'
+    
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        form.save()
 
-    r = requests.get(url.format(city)).json()
-    #print(r.text)
+    form = CityForm()
 
-    city_weather = {
-        'city' : city,
-        'temperature' : r['main']['temp'],
-        'description' : r['weather'][0]['description'],
-        'icon' : r['weather'][0]['icon'],
-    }
+    cities = City.objects.all()
 
-    print(city_weather)
+    weather_data = []
 
-    context = {'city_weather' : city_weather}
+    for city in cities:
+        r = requests.get(url.format(city)).json()
+        #print(r.text)
+
+        city_weather = {
+            'city' : city.name,
+            'temperature' : r['main']['temp'],
+            'description' : r['weather'][0]['description'],
+            'icon' : r['weather'][0]['icon'],
+        }
+
+        weather_data.append(city_weather)
+
+    print(weather_data)
+
+    context = {'weather_data' : weather_data, 'form' : form}
     return render(request, 'the_weather/weather.html', context)
