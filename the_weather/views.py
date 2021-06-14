598f005ad178793a2a76351cda46fe53d313f925
thm1d <tahmid11q@gmail.com>
2021-06-14 20:51:44 +0600

    Dynamic view /w Weather Api

--- the_weather/views.py
+++ the_weather/views.py
@@ -1,4 +1,22 @@
+import requests
 from django.shortcuts import render
+from decouple import config
 
 def index(request):
-    return render(request, 'the_weather/weather.html')
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + config('API_KEY')
+    city = 'Pabna'
+
+    r = requests.get(url.format(city)).json()
+    #print(r.text)
+
+    city_weather = {
+        'city' : city,
+        'temperature' : r['main']['temp'],
+        'description' : r['weather'][0]['description'],
+        'icon' : r['weather'][0]['icon'],
+    }
+
+    print(city_weather)
+
+    context = {'city_weather' : city_weather}
+    return render(request, 'the_weather/weather.html', context)
