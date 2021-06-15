148c410d15913daae75c31701e3332d282aec7e4
thm1d <tahmid11q@gmail.com>
2021-06-15 23:22:11 +0600

    Invalid input msg & Delete Function

--- the_weather/views.py
+++ the_weather/views.py
@@ -1,5 +1,5 @@
 import requests
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from decouple import config
 from .models import City
 from .forms import CityForm
@@ -7,9 +7,31 @@ from .forms import CityForm
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + config('API_KEY')
     
+    err_msg = ''
+    message = ''
+    message_class = ''
+
     if request.method == 'POST':
         form = CityForm(request.POST)
-        form.save()
+
+        if form.is_valid():
+            new_city = form.cleaned_data['name']
+            city_count = City.objects.filter(name=new_city.capitalize()).count()
+            if city_count == 0:
+                r = requests.get(url.format(new_city)).json()
+                if r['cod'] == 200:
+                    form.save()
+                else:
+                    err_msg = "The City doesn't exist!"
+            else:
+                err_msg = 'The City is already added!'
+        
+        if err_msg:
+            message = err_msg
+            message_class = 'is-danger'
+        else:
+            message = 'City added successfully!'
+            message_class = 'is-success'
 
     form = CityForm()
 
@@ -30,7 +52,18 @@ def index(request):
 
         weather_data.append(city_weather)
 
-    print(weather_data)
+    #print(weather_data)
 
-    context = {'weather_data' : weather_data, 'form' : form}
+    context = {
+        'weather_data' : weather_data, 
+        'form' : form, 
+        'message' : message, 
+        'message_class' : message_class
+    }
+
     return render(request, 'the_weather/weather.html', context)
+
+def delete_city(request, city_name):
+    City.objects.get(name=city_name).delete()
+    return redirect('home')
+
